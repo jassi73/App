173a728e14188d1fcfe89dbd23af7f2be20d9aa8
JASA RAM <jessparihar73@gmail.com>
2021-04-30 00:44:54 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+// import User from "./User";
+import "./App.css";
+
+const User = lazy(() => import("./User"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div> User Profile Is Loading</div>}>
+        <User></User>
+      </Suspense>
     </div>
   );
 }
